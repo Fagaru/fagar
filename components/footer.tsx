92be20f2cd4e8ab3695ec54f12133e025c379d08
stanislas.bicaba <stanislas.bicaba@ten.com>
2024-06-29 20:41:17 +0200

    svg file

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+const Footer = ()=>{
+    return (
+        <footer className="relative left-0 right-0 bottom-0 w-full p-1 sm:px-4 bg-white ">
+            <div className="nx-auto py-10">
+                <p className="text-center  text_xs  text-black">
+                    &copy; 2023 Fagaru, Inc. All rights reserved.
+
+                </p>
+
+            </div>
+
+        </footer>
+
+    );
+}
+export default Footer;
