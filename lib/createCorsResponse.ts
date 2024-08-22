d15c5e6f1c4502ed7a48487d4ec4355e364ba22b
abdoulaye14 <abdoulaye19971@gmail.com>
2024-08-22 20:52:32 +0200

    cors commented on backend

--- lib/createCorsResponse.ts
+++ lib/createCorsResponse.ts
@@ -23,11 +23,11 @@ export function createCorsResponse(
 ) {
   return new NextResponse(JSON.stringify(body), {
     status: options.status,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Authorization, Content-Type, Origin, Accept", // Ajouter les en-têtes nécessaires seulement
-      "Access-Control-Max-Age": "86400", // Pour mettre en cache la réponse préliminaire
-    },
+    // headers: {
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    //   "Access-Control-Allow-Headers": "Authorization, Content-Type, Origin, Accept", // Ajouter les en-têtes nécessaires seulement
+    //   "Access-Control-Max-Age": "86400", // Pour mettre en cache la réponse préliminaire
+    // },
   });
 }
